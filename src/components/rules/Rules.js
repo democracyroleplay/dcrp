397d6c3f0b4bb1f3b0f893ce53f40731749c91c2
democracyroleplay <democracyroleplay@gmail.com>
2023-11-07 16:55:41 +0530

    added new rules

--- src/components/rules/Rules.js
+++ src/components/rules/Rules.js
@@ -1,107 +1,185 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import "./Rules.css";
 import rulesData from "../../rulesData/rules.json";
+import rpRules from "../../rulesData/rpRules.json";
+import pdCivilianRules from "../../rulesData/pdCivilianRules.json";
+import exclusiveRules from "../../rulesData/exclusiveRules.json";
 import Loader from "react-loaders";
 import Particles from "react-tsparticles";
 import { loadSlim } from "tsparticles-slim";
 
 const Rules = () => {
-
   const particlesInit = useCallback(async (engine) => {
     console.log(engine);
     await loadSlim(engine);
   }, []);
 
-  const particlesLoaded = useCallback(async (container) => {
-  }, []);
+  const particlesLoaded = useCallback(async (container) => {}, []);
 
+  const [activeRule, setActiveRule] = useState(0);
 
   return (
     <>
       <div class="rules active">
-        <p class="rules-title">Rules</p>
+        <div className="pageTitle">
+          <p
+            class="rules-title"
+            style={{
+              textDecoration: activeRule === 0 ? "underline" : null,
+              cursor: "pointer",
+            }}
+            onClick={() => setActiveRule(0)}
+          >
+            General Rules
+          </p>
+          <p
+            class="rules-title"
+            style={{
+              textDecoration: activeRule === 3 ? "underline" : null,
+              cursor: "pointer",
+            }}
+            onClick={() => setActiveRule(3)}
+          >
+            Exclusive Rules(DCRP)
+          </p>
+          <p
+            class="rules-title"
+            style={{
+              textDecoration: activeRule === 1 ? "underline" : null,
+              cursor: "pointer",
+            }}
+            onClick={() => setActiveRule(1)}
+          >
+            Roleplay Rules
+          </p>
+          <p
+            class="rules-title"
+            style={{
+              textDecoration: activeRule === 2 ? "underline" : null,
+              cursor: "pointer",
+            }}
+            onClick={() => setActiveRule(2)}
+          >
+            PD Civilian interaction Rules
+          </p>
+        </div>
         <div class="gradient-cards">
-          {rulesData.rules.map((item) => (
-            <div class="card">
-              {" "}
-              <div class="rules-card bg-green-box">
-                <p class="card-title">{item.title}</p>
-                <p class="card-description">{item.description}</p>
-              </div>
-            </div>
-          ))}
+          {activeRule === 0
+            ? rulesData.rules.map((item) => (
+                <div class="card">
+                  {" "}
+                  <div class="rules-card bg-green-box">
+                    <p class="card-title">{item.title}</p>
+                    <p class="card-description">{item.description}</p>
+                  </div>
+                </div>
+              ))
+            : null}
+          {activeRule === 1
+            ? rpRules.rules.map((item) => (
+                <div class="card">
+                  {" "}
+                  <div class="rules-card bg-green-box">
+                    <p class="card-title">{item.title}</p>
+                    <p class="card-description">{item.description}</p>
+                  </div>
+                </div>
+              ))
+            : null}
+          {activeRule === 2
+            ? pdCivilianRules.rules.map((item) => (
+                <div class="card">
+                  {" "}
+                  <div class="rules-card bg-green-box">
+                    <p class="card-title">{item.title}</p>
+                    <p class="card-description">{item.description}</p>
+                  </div>
+                </div>
+              ))
+            : null}
+          {activeRule === 3
+            ? exclusiveRules.rules.map((item) => (
+                <div class="card">
+                  {" "}
+                  <div class="rules-card bg-green-box">
+                    <p class="card-title">{item.title}</p>
+                    <p class="card-description">{item.description}</p>
+                  </div>
+                </div>
+              ))
+            : null}
         </div>
       </div>
       <Loader type="pacman" />
       <Particles
-            id="tsparticles"
-            init={particlesInit}
-            loaded={particlesLoaded}
-            options={{
-              fpsLimit: 144,
-              interactivity: {
-                events: {
-                  onClick: {
-                    enable: true,
-                    mode: "push",
-                  },
-                  onHover: {
-                    enable: true,
-                    mode: "repulse",
-                  },
-                  resize: true,
-                },
-                modes: {
-                  push: {
-                    quantity: 4,
-                  },
-                  repulse: {
-                    distance: 300,
-                    duration: 2,
-                  },
-                },
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          fpsLimit: 144,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
               },
-              particles: {
-                color: {
-                  value: "#00ffee",
-                },
-                links: {
-                  color: "#00ffb3",
-                  distance: 120,
-                  enable: true,
-                  opacity: 0.3,
-                  width: 1,
-                },
-                move: {
-                  direction: "auto",
-                  enable: true,
-                  outModes: {
-                    default: "bounce",
-                  },
-                  random: false,
-                  speed: 6,
-                  straight: false,
-                },
-                number: {
-                  density: {
-                    enable: true,
-                    area: 500,
-                  },
-                  value: 25,
-                },
-                opacity: {
-                  value: 0.5,
-                },
-                shape: {
-                  type: "circle",
-                },
-                size: {
-                  value: { min: 1, max: 5 },
-                },
+              onHover: {
+                enable: true,
+                mode: "repulse",
               },
-              detectRetina: true,
-            }}
-          />
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 300,
+                duration: 2,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#00ffee",
+            },
+            links: {
+              color: "#00ffb3",
+              distance: 120,
+              enable: true,
+              opacity: 0.3,
+              width: 1,
+            },
+            move: {
+              direction: "auto",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 6,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 500,
+              },
+              value: 25,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
     </>
   );
 };
